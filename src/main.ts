459f9c215cc68f77efc0a6e7cad3b4f71d750625
florian.heidemann <florian.heidemann@codered.net>
2025-12-07 11:37:11 +0100

    add navigation styling and functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import './assets/css/index.css'
+
+const navigationToggle: HTMLElement | null = document.querySelector("[data-navigation-toggle]");
+const navigation: HTMLElement | null = document.querySelector("[data-navigation]");
+const navigationItems: NodeListOf<HTMLAnchorElement> = document.querySelectorAll("[data-navigation] a");
+
+navigationItems.forEach((element: HTMLAnchorElement) => element.addEventListener("click", toggleNavigation));
+navigationToggle?.addEventListener("click", toggleNavigation);
+
+function toggleNavigation (): void {
+    if (!navigation) return;
+    navigation.classList.toggle("navigation--hidden");
+    if(!navigation.classList.contains("navigation--hidden")) {
+     navigation.setAttribute("aria-expanded", "true")
+    } else {
+     navigation.setAttribute("aria-expanded", "false")
+    }
+}
